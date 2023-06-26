d1e099da7ea2ebde9696f5af259caa9452bb9ab3
Chris <137030690+Chr1s333@users.noreply.github.com>
2023-06-26 12:45:15 +0530

    Update script.js

--- Task 3 - Temperature Converter/script.js
+++ Task 3 - Temperature Converter/script.js
@@ -6,21 +6,16 @@ celsius.oninput = function () {
     fahrenheit.value = parseFloat(f.toFixed(2));  
     let k = (parseFloat(celsius.value) + 273.15);
     kelvin.value = parseFloat(k.toFixed(2));
-    }
+}
 fahrenheit.oninput = function () {
-    let c = ((parseFloat(
-        fahrenheit.value) - 32) * 5) / 9;
-        celsius.value = parseFloat(c.toFixed(2));
-      
-        let k = (parseFloat(
-            fahrenheit.value) - 32) * 5 / 9 + 273.15;
-        kelvin.value = parseFloat(k.toFixed(2));
-    }
-    kelvin.oninput = function () {
-        let f = (parseFloat(
-            kelvin.value) - 273.15) * 9 / 5 + 32;
-        fahrenheit.value = parseFloat(f.toFixed(2));
-      
-        let c = (parseFloat(kelvin.value) - 273.15);
-        celsius.value = parseFloat(c.toFixed(2));
-    }
+    let c = ((parseFloat(fahrenheit.value) - 32) * 5) / 9;
+    celsius.value = parseFloat(c.toFixed(2));
+    let k = (parseFloat(fahrenheit.value) - 32) * 5 / 9 + 273.15;
+    kelvin.value = parseFloat(k.toFixed(2));
+}
+kelvin.oninput = function () {
+    let f = (parseFloat(kelvin.value) - 273.15) * 9 / 5 + 32;
+    fahrenheit.value = parseFloat(f.toFixed(2));
+    let c = (parseFloat(kelvin.value) - 273.15);
+    celsius.value = parseFloat(c.toFixed(2));
+}
